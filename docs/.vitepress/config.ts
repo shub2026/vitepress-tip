f6dceb1d186305aaf2c566829f93d7d1b36106c0
shub2026 <shub2026@gmail.com>
2026-06-08 20:29:45 +0800

    新增KEC课程管理平台说明文档及侧边栏链接

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -163,6 +163,7 @@ export default withMermaid(
             { text: 'Lightroom处理流程与技巧', link: '/other/lightroom-workflow-and-tips' },
             { text: 'Lightroom预设使用指南', link: '/other/lightroom-preset-guide' },
             { text: 'WPS删除右键新建旧版菜单', link: '/other/wps-del' },
+            { text: 'KEC课程管理平台说明', link: '/other/kec-manager' },
           ],
         },
       ],
